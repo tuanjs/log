c5689561c6db28260c4a236cfc9b61a028253ce6
joriewong <stillba@icloud.com>
2021-07-14 11:13:34 +0800

    feat: timer use ref

--- src/Log/index.tsx
+++ src/Log/index.tsx
@@ -1,32 +1,43 @@
-import {FC, useEffect, useState} from 'react'
-import CodeMirror, {IReactCodemirror} from '@uiw/react-codemirror'
-import 'codemirror/keymap/sublime'
-import 'codemirror/theme/monokai.css'
+import { FC, useEffect, useState, useRef } from "react";
+import CodeMirror, { IReactCodemirror } from "@uiw/react-codemirror";
+import "codemirror/keymap/sublime";
+import "codemirror/theme/monokai.css";
 
 export interface ILog extends IReactCodemirror {
-	polling?: number
-	request?: (params?: any) => Promise<any>
+  polling?: number;
+  request?: (params?: any) => Promise<any>;
 }
 
-let timer: NodeJS.Timeout | null = null
+export type Timer = NodeJS.Timeout | null;
 
-const Log:FC<ILog> = ({polling, request, ...rest}) => {
-	const [value, setValue] = useState()
+const Log: FC<ILog> = ({ polling, request, ...rest }) => {
+	const timer = useRef<Timer>(null)
+  const [value, setValue] = useState();
 
-	useEffect(() => {
-		if(!polling) return
-		if(!request) return
-		timer = setInterval(async () => {
-			const {data} = await request()
-			setValue(data)
-		}, polling)
-		return () => {
-      timer && clearInterval(timer)
-      timer = null
-    }
-	}, [polling, request])
+  useEffect(() => {
+    if (!polling) return;
+    if (!request) return;
+    timer.current = setInterval(async () => {
+      const { data } = await request();
+      setValue(data);
+    }, polling);
+    return () => {
+      timer.current && clearInterval(timer.current);
+      timer.current = null;
+    };
+  }, [polling, request]);
 
-	return <CodeMirror {...rest} value={value}/>
-}
+  return (
+    <CodeMirror
+      value={value}
+      options={{
+        theme: "monokai",
+        keyMap: "sublime",
+        readOnly: true,
+      }}
+      {...rest}
+    />
+  );
+};
 
-export default Log
+export default Log;
